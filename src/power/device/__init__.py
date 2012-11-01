33ca1cfca5b08947e0f5413297d9d4fda1a31b93
chathawa <chathawa@f2234473-aea0-4c34-a049-d44cbc1b6343>
2012-11-01 17:31:04 +0000

    Setup some settings for to use during development, tested the __init__ stuff to make sure it would work.
    For some reason, device/__init__.py is called twice. This might be a problem. I don't know
    if it is being called twice from one invocation, or if it is being run twice. We may need
    to have a static variable (?) that allows to track if this code has been run yet.
    
    
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@94 f2234473-aea0-4c34-a049-d44cbc1b6343

---
+++ src/power/device/__init__.py
@@ -0,0 +1,1 @@
+print "Initing the Device module..."
--- src/power/power/settings.py
+++ src/power/power/settings.py
@@ -1,7 +1,10 @@
 # Django settings for power project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_ROOT = os.path.join('/', *os.path.realpath(__file__).split(os.sep)[:-2])
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -11,9 +14,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'power',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -24,7 +27,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -56,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_ROOT + '/collected_static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT + '/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -106,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT + '/templates/'
 )
 
 INSTALLED_APPS = (
@@ -116,9 +121,17 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'device',
+    'factory_reset',
+    'power',
+    'power_bill',
+    'power_data',
+    'rest_api',
+    'satellite',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
