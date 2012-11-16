c2adfcfa4f4156a45ba3cfe55cf06a7ea09ff7b5
gdegeus <gdegeus@f2234473-aea0-4c34-a049-d44cbc1b6343>
2012-11-16 20:25:04 +0000

    Another attempt to fix everything.
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@165 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/power/power_data/models.py
+++ src/power/power_data/models.py
@@ -4,7 +4,7 @@ from device.models import Device
 
 class Data(models.Model):
 	device_id = models.ForeignKey(Device)
-	timestamp = models.DateField(auto_now_add=True)
-	watt = models.DecimalField(max_digits=6, decimal_places=2)
-	avg_volt = models.DecimalField(max_digits=4, decimal_places=1)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	watt = models.DecimalField(max_digits=6, decimal_places=4)
+	avg_volt = models.DecimalField(max_digits=4, decimal_places=4)
 	interval = models.IntegerField()
--- src/power/satellite/models.py
+++ src/power/satellite/models.py
@@ -4,4 +4,6 @@ from device.models import Device
 
 class Satellite(models.Model):
 	serial_number = models.CharField(max_length=9)
+	outlet = models.CharField(max_length=9)
+	unique_together = ("serial_number", "outlet")
 	device_id = models.ForeignKey(Device)
