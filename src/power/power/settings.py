e11d1f71779fce9d03f26649a3f5b271b32e7f26
chathawa <chathawa@f2234473-aea0-4c34-a049-d44cbc1b6343>
2012-11-30 21:15:35 +0000

    Addressed an issue caused by PROJECT_ROOT variable on Windows.
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@204 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/power/power/settings.py
+++ src/power/power/settings.py
@@ -4,7 +4,13 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-PROJECT_ROOT = os.path.join('/', *os.path.realpath(__file__).split(os.sep)[:-2])
+PROJECT_ROOT = os.path.join(*os.path.realpath(__file__).split(os.sep)[:-2])
+if os.path.splitdrive(PROJECT_ROOT)[0] != '':
+    PROJECT_ROOT = os.path.splitdrive(PROJECT_ROOT)[0] + os.sep + os.path.splitdrive(PROJECT_ROOT)[1]
+
+print PROJECT_ROOT
+print os.path.realpath(__file__).split(os.sep)[:-2]
+print os.path.splitdrive(PROJECT_ROOT)[0]
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
