6cab41b29d2ea5498f1ee921e2c107d0ebab38b3
chathawa <chathawa@f2234473-aea0-4c34-a049-d44cbc1b6343>
2012-12-12 05:08:01 +0000

    Added a few things to the device model, admin. Also enabled breadcrumbs in the things.
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@304 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/power/device/admin.py
+++ src/power/device/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from device.models import Device
+from device.models import Device, DeviceGroup
 
 class DeviceAdmin(admin.ModelAdmin):
     add_form_template = 'device/add.html'
 
+    list_display = ('name', 'enabled', )
+    list_filter = ('enabled',)
+
+    def changelist_view(self, request, extra_context=None):
+        if not request.GET.has_key('enabled__exact'):
+            q = request.GET.copy()
+            q['enabled__exact'] = '1'
+            request.GET = q
+            request.META['QUERY_STRING'] = request.GET.urlencode()
+        return super(DeviceAdmin,self).changelist_view(request, extra_context=extra_context)
+
+class DeviceGroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('devices', )
+
 admin.site.register(Device, DeviceAdmin)
+admin.site.register(DeviceGroup, DeviceGroupAdmin)
--- src/power/device/models.py
+++ src/power/device/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 class Device(models.Model):
     name = models.CharField(max_length=500)
+    enabled = models.BooleanField(default=True)
 	
+    def __unicode__(self):
+        return self.name
+
+class DeviceGroup(models.Model):
+    name = models.CharField(max_length=500)
+    devices = models.ManyToManyField(Device)
+
     def __unicode__(self):
         return self.name
