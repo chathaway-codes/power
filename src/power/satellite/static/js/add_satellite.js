c04acc7a29fd3fc0c0c88f505d3ecafec2126498
chathawa <chathawa@f2234473-aea0-4c34-a049-d44cbc1b6343>
2012-12-12 04:39:49 +0000

    Now generating a random satellite SN!
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@303 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/power/satellite/static/js/add_satellite.js
+++ src/power/satellite/static/js/add_satellite.js
@@ -7,7 +7,15 @@ define(["jquery", "backbone", "v/add_satellite", "backbone-tastypie"], function(
     var searching = function() {
         console.log("Searching done now");
         setTimeout(function() {
-            App.model.set('serial_number', 'AAA-AAA-AAA');
+            // Generate a random serial
+            var possible = "ABCDEF0123456789";
+            var sn = "";
+            for( var i=0; i < 9; i++ ) {
+                if(i % 3 == 0 && i != 0)
+                    sn += "-";
+                sn += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            App.model.set('serial_number', sn);
             App.nextState();
             clearTimeout(this);
         }, 4000);
