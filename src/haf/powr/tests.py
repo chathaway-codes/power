9d4592d8d9e1638f8d5e1a968dcedd8740194e00
chathawa <chathawa@f2234473-aea0-4c34-a049-d44cbc1b6343>
2013-01-29 18:36:53 +0000

    Added some tests
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@384 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/haf/powr/tests.py
+++ src/haf/powr/tests.py
@@ -7,6 +7,19 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+class CheckResourceMeta(TestCase):
+    def test_authentication(self):
+        import sys
+        import inspect
+        from tastypie.resources import ModelResource
+        from rest_api.resources import ModelMeta
+        from powr import resources
+        
+        for name, obj in inspect.getmembers(sys.modules[resources.__name__]):
+            if inspect.isclass(obj) and issubclass(obj, ModelResource):
+                self.assertTrue(issubclass(obj.Meta, ModelMeta), 
+                                "%s.%s.Meta: Resources should inherit the meta options from ModelMeta"
+                                 % (obj.__module__, obj.__name__))
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
--- src/haf/rest_api/resources.py
+++ src/haf/rest_api/resources.py
@@ -2,9 +2,11 @@ from tastypie.resources import ModelResource as def_ModelResource
 from rest_api.authentication import SessionAuthentication
 from tastypie.authorization import DjangoAuthorization
 
-class ModelResource(def_ModelResource):
-    pass
 class ModelMeta:
     always_return_data = True
     authentication = SessionAuthentication()
     authorization = DjangoAuthorization()
+
+class ModelResource(def_ModelResource):
+    class Meta(ModelMeta):
+        pass
