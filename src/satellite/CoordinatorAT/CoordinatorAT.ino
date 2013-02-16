bd41222926ef3efe99090d572a6fecb2ae5ca98e
nquimby <nquimby@f2234473-aea0-4c34-a049-d44cbc1b6343>
2013-02-16 05:12:08 +0000

    Satellite codes.
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@399 f2234473-aea0-4c34-a049-d44cbc1b6343

---
+++ src/satellite/CoordinatorAT/CoordinatorAT.ino
@@ -0,0 +1,26 @@
+/*
+Receives data from a software serial port that is connected
+to a radio, prints data to hard serial port.
+
+Radio: XBee Series 2
+Firmware: ZigBee Coordnator AT
+*/
+
+#include<SoftwareSerial.h>
+
+SoftwareSerial XBS(10, 11);
+
+
+char readChar;
+void setup(){
+  Serial.begin(9600);
+  XBS.begin(9600);
+}
+
+void loop(){
+  if(XBS.available() > 0){
+    readChar = XBS.read();
+    Serial.print(readChar);    
+  }
+  delay(100);
+}
