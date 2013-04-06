0eac85f04a141ebd1d1bbe5d72be3ee7e265a9cf
nquimby <nquimby@f2234473-aea0-4c34-a049-d44cbc1b6343>
2013-04-06 02:43:41 +0000

    Satellite codes V2
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@481 f2234473-aea0-4c34-a049-d44cbc1b6343

---
+++ src/satellite/SatelliteATv2/powrDevice/powrDevice.cpp
@@ -0,0 +1,36 @@
+/*
+powrDevice.cpp
+*/
+
+#include "Arduino.h"
+#include "powrDevice.h"
+
+
+powrDevice::powrDevice(int cPin, int vPin, int outletID)
+{
+	pinMode(cPin, INPUT);
+	pinMode(vPin, INPUT);
+	_cPin = cPin;
+	_vPin = vPin;
+	_outletID = outletID;
+    payLoad = String("");
+}
+
+String powrDevice::buildPayLoad(String radioID)
+{
+  //Clear out payLoad
+  payLoad = "powr:" + radioID + ":" + _outletID + ":";
+  
+  //Take a reading from both ADCs
+  _cVal = analogRead(_cPin);
+  _vVal = analogRead(_vPin);
+  
+  //Figure out real numbers
+  voltage = map(_vVal, 0, 650, 0, 140);
+  //voltage = _vVal;
+  current = _cVal;
+  
+  payLoad = payLoad + String(voltage, HEX) + ":" + String(current, HEX);
+  return payLoad;
+}
+
---
+++ src/satellite/SatelliteATv2/powrDevice/powrDevice.h
@@ -0,0 +1,28 @@
+/*
+powrDevice.h
+*/
+
+#ifndef powrDevice_h
+#define powrDevice_h
+
+#include "Arduino.h"
+
+class powrDevice
+ {
+  private: 
+    int _cPin;
+    int _vPin;
+    int _cVal;
+    int _vVal;
+	int _outletID;
+    String payLoad;
+    boolean enable; //toggles reporting for an outlet
+    
+  public:
+    int current;
+    int voltage;
+    powrDevice(int cPin, int vPin, int outletID);
+    String buildPayLoad(String radioID);
+   };
+ 
+ #endif
