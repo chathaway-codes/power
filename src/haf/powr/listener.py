e1b4c2ef0e83f35cc0688012456ed880ee2e39eb
chathawa <chathawa@f2234473-aea0-4c34-a049-d44cbc1b6343>
2013-04-07 23:31:40 +0000

    Small changes
    git-svn-id: svn://dunari.cis.vtc.edu/POWER@489 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/haf/powr/listener.py
+++ src/haf/powr/listener.py
@@ -1,18 +1,21 @@
-from powr.models import Data, PowerCost
+from powr.models import Data, PowerCost, Device
 
 def process(satellite, rest):
     print "process(%s, %s)" % (satellite, rest)
     
     # Get all the data we need
     voltage, current = rest.split(':')
-    voltage = int(voltage, 16)
-    current = int(current, 16)
+    voltage = int(voltage, 10)
+    current = int(current, 10) / 1000.0
     
     # Find the correct PowerCost for now
     power_cost = PowerCost.find_power_cost()
     
     # Get the device
-    device = satellite.device_set.all()[0]
+    satellites = satellite.device_set.all()
+    device = Device.objects.all()[0]
+    if len(satellites) > 0:
+        device = satellite.device_set.all()[0]
     
     # Create and save the power data
     Data(device_id=device, watt=voltage*current, avg_volt=voltage, interval=5, cost=power_cost).save()
