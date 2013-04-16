ebe56c139cca333e4a99f2238b06f206e52742f5
nquimby <nquimby@f2234473-aea0-4c34-a049-d44cbc1b6343>
2013-04-16 00:33:34 +0000

    git-svn-id: svn://dunari.cis.vtc.edu/POWER@519 f2234473-aea0-4c34-a049-d44cbc1b6343

--- src/satellite/CoordinatorAT/CoordinatorAT.ino
+++ src/satellite/CoordinatorAT/CoordinatorAT.ino
@@ -1,26 +1,32 @@
 /*
-Receives data from a software serial port that is connected
-to a radio, prints data to hard serial port.
-
-Radio: XBee Series 2
-Firmware: ZigBee Coordnator AT
-*/
-
+	Receives data from a software serial port that is connected
+	to a radio, prints data to hard serial port.
+	
+	Radio: XBee Series 2
+	Firmware: ZigBee Coordnator AT
+	*/
+	
 #include<SoftwareSerial.h>
-
-SoftwareSerial XBS(10, 11);
-
-
-char readChar;
-void setup(){
-  Serial.begin(9600);
-  XBS.begin(9600);
-}
-
-void loop(){
-  if(XBS.available() > 0){
-    readChar = XBS.read();
-    Serial.print(readChar);    
-  }
-  delay(100);
-}
+	
+	SoftwareSerial XBS(10, 11);
+	SoftwareSerial LCDS(7, 6);
+	byte setcursorLCD = 12;
+        
+	char readChar;
+	void setup(){
+	  Serial.begin(9600);
+	  XBS.begin(9600);
+          LCDS.begin(9600);
+          LCDS.listen();
+          // Cursor move command
+          LCDS.write(setcursorLCD);
+          LCDS.print("IP: 192.168.1.187");
+          XBS.listen();
+	}
+	
+	void loop(){
+	  if(XBS.available() > 0){
+	    readChar = XBS.read();
+	    Serial.print(readChar);   
+	  }
+	}
